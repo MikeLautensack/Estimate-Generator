ca6f536be92e90e321305049caf1ca2cf7e232e4
Mike <mikelautensack100@gmail.com>
2023-01-26 22:50:53 -0500

    started work on the fe

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login'
+import React, { useState } from 'react';
 
 function App() {
+
+  //const [user, setUser] = useState
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center bg-blue-100 h-screen">
+      <div className='text-center w-[80%]'>
+        <h3>Welcome</h3>
+        <h6>to</h6>
+        <h1>Estimate Generator</h1>
+      </div>
+      <Login />
     </div>
   );
 }
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Login = () => {
+    //const [username, setUsername] = useState
+    //const [email, setEmail] = useState
+    //const [password, setPassword] = useState
+//
+    //const setUsername = ()=> {
+//
+    //}
+  return (
+    <form className='grid bg-blue-600 border rounded border-blue-600 p-5'>
+        <label className='' for='username'>Username</label>
+        <input className='my-1 px-1.5 py-0.5' /*value={username}*/ type='name' placeholder='Username' id='name' name='name'></input>
+        <label className='' for='email'>Email</label>
+        <input className='my-1 px-1.5 py-0.5' /*value={email}*/ type='email' placeholder='email@gmail.com' id='email' name='email'></input>
+        <label className='' for='password'>Password</label>
+        <input className='my-1 px-1.5 py-0.5' /*value={password}*/ type='password' placeholder='password' id='password' name='password'></input>
+        <button className='bg-orange-400 border-orange-400 border rounded border-blue-500 my-1'>Log In</button>
+        <button className='bg-orange-400 border-orange-400 border rounded border-blue-500 my-1'>Log In As Guest</button>
+        <button className='bg-orange-400 border-orange-400 border rounded border-blue-500 my-1'>Create an Accout</button>
+    </form>
+  )
+}
+
+export default Login
