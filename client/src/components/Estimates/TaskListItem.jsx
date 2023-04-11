d04104c6a2a9cc98b0130cdcadd7e399fb432016
Mike <mikelautensack100@gmail.com>
2023-04-10 20:39:41 -0400

    working on css

--- client/src/components/Estimates/TaskListItem.jsx
+++ client/src/components/Estimates/TaskListItem.jsx
@@ -4,17 +4,17 @@ import SubtaskListItem from './SubtaskListItem'
 
 const TaskListItem = ({ task }) => {
   return (
-    <div>
-        <h1>{task.taskName}</h1>
-        <h3>{task.taskDescription}</h3>
-        <ul>
-            {task.subtasks.map((subtask) => {
+    <div className='task-list-item'>
+        <h1 className='task-list-item-taskname'>{task.taskName}</h1>
+        <h3 className='task-list-item-heading'>{task.taskDescription}</h3>
+        <ul className='list-of-subtasks'>
+            {task.subtasks.map((subtask) => (
                 <li>
                     <SubtaskListItem subtask={subtask}/>
                 </li>
-            })}
+            ))}
         </ul>
-        <h2>{task.total ? `$${task.total}` : '$0.00'}</h2>
+        <h2 className='task-list-item-total'>{task.total ? `$${task.total}` : '$0.00'}</h2>
     </div>
   )
 }
