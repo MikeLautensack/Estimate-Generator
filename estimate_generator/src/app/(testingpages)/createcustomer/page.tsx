68d0bc859e706e07d06c1b1456c0ea86869bfbec
Mike <mikelautensack100@gmail.com>
2023-11-04 18:59:30 -0400

    testing error fix

--- estimate_generator/src/app/(testingpages)/createcustomer/page.tsx
+++
@@ -1,9 +0,0 @@
-import CustomerForm from "@/components/forms/CustomerForm";
-
-export default function page() {
-  return (
-    <div className="flex justify-center items-center bg-primary300 h-screen">
-        <CustomerForm />
-    </div>
-  )
-}
--- estimate_generator/src/app/(testingpages)/createestimate/page.tsx
+++
@@ -1,9 +0,0 @@
-import EstimateTestForm from '../../../components/forms/EstimateTestForm'
-
-export default function page() {
-  return (
-    <div className="flex justify-center items-center bg-primary300 h-screen">
-        <EstimateTestForm />
-    </div>
-  )
-}
