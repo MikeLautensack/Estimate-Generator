0479cb1ecb569a7c7e0af8e6f95227b5d449d1a6
Mike <mikelautensack100@gmail.com>
2023-11-04 17:24:29 -0400

    Merge alpha into main

--- estimate_generator/src/app/(testingpages)/profileform/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProfileForm from "@/components/forms/ProfileForm";
-
-export default function page() {
-  return (
-    <div className="flex justify-center items-center bg-primary300 h-screen">
-        <ProfileForm />
-    </div>
-  )
-}
