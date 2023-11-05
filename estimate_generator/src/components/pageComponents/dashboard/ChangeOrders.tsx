30064ad2f36d022eb58d2e90d8e02726574972e2
Mike <mikelautensack100@gmail.com>
2023-11-04 20:22:34 -0400

    Attempting to fix type error in change orders table data

--- estimate_generator/src/components/pageComponents/dashboard/ChangeOrders.tsx
+++ estimate_generator/src/components/pageComponents/dashboard/ChangeOrders.tsx
@@ -3,7 +3,6 @@ import ChangeOrdersTable from "../../tables/changeOrderTable/ChangeOrdersTable"
 import { columns } from '../../tables/changeOrderTable/columns'
 import { ChangeOrders } from "@/types/changeOrders"
 import { changeOrders } from "@/db/schemas/changeOrders"
-import { useEffect, useState } from "react"
 
 async function getChangeOrders() {
   try {    
@@ -17,10 +16,9 @@ async function getChangeOrders() {
 
 export default async function ChangeOrders() {
   const data = await getChangeOrders()
-  const [ changeOrders, setChangeOrders ] = useState(data!)
   return (
     <div>
-      <ChangeOrdersTable columns={columns} data={changeOrders} />
+      <ChangeOrdersTable columns={columns} data={data!} />
     </div>
   )
 }
