e8ac3ddb70fa6689891d2242db0a776469ff9cff
Mike <mikelautensack100@gmail.com>
2023-11-16 22:55:17 -0500

    Implemented role based redirect on login

---
+++ estimate_generator/src/app/api/redirect/route.ts
@@ -0,0 +1,17 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { NextRequest, NextResponse } from "next/server";
+import { redirect } from "next/navigation";
+
+
+export async function GET(request: NextRequest) {
+    const session = await getServerSession(authOptions)
+    if(session.user.role === 'admin') {
+        return NextResponse.redirect(new URL ('http://localhost:3000/admin-dashboard'))
+    } else if (session.user.role === 'contractor') {
+        return NextResponse.redirect(new URL ('http://localhost:3000/contractor-dashboard'))
+    } else if (session.user.role === 'customer') {
+        return NextResponse.redirect(new URL ('http://localhost:3000/customer-dashboard'))
+    }
+    return new NextResponse('redirecting...')
+}
