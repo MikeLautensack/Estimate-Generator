09050d8ec260972ba9e13fa273e91950e688e64a
Mike <mikelautensack100@gmail.com>
2023-11-20 08:19:51 -0500

    Trying to fix build error from resend and/or react-email

--- estimate_generator/src/utils/sendVerificationRequest.ts
+++ estimate_generator/src/utils/sendVerificationRequest.ts
@@ -9,7 +9,7 @@ export async function sendVerificationRequest(params: SendVerificationRequestPar
 
     try {
         const data = await resend.emails.send({
-            from: 'Testing react email render <onboarding@resend.dev>',
+            from: 'Testing react email ..... <onboarding@resend.dev>',
             to: [identifier],
             subject: `Log in to ${host}`,
             text: text({ url, host }),
