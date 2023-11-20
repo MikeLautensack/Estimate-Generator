2e96a2fcef0f7c6eae649b73405373e669693ca5
Mike <mikelautensack100@gmail.com>
2023-11-20 18:08:40 -0500

    Trying to fix build error from resend and/or react-email

--- estimate_generator/src/utils/sendVerificationRequest.ts
+++ estimate_generator/src/utils/sendVerificationRequest.ts
@@ -1,4 +1,5 @@
 import EstimateEmail from "@/emails/EstimateEmail"
+import { renderAsync } from "@react-email/components"
 import { SendVerificationRequestParams } from "next-auth/providers/email"
 import { Resend } from "resend"
 
@@ -7,13 +8,15 @@ export async function sendVerificationRequest(params: SendVerificationRequestPar
     const { host } = new URL(url)
     const resend = new Resend(process.env["EMAIL_KEY"])
 
+    const html = await renderAsync(EstimateEmail({ url, host }))
+
     try {
         const data = await resend.emails.send({
             from: 'Testing react email ..... <onboarding@resend.dev>',
             to: [identifier],
             subject: `Log in to ${host}`,
             text: text({ url, host }),
-            react: EstimateEmail({ url, host })
+            html: html
         })
     } catch (error) {
         throw new Error('Failed to send the verification email.')
