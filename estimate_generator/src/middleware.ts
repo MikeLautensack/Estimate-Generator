eb86e4d98000246838119c61c7b11ec8bc939303
Mike <mikelautensack100@gmail.com>
2023-11-21 18:18:22 -0500

    Trying to fix build error from resend and/or react-email

--- estimate_generator/src/middleware.ts
+++ estimate_generator/src/middleware.ts
@@ -1,16 +1,2 @@
-import { withAuth, NextRequestWithAuth } from 'next-auth/middleware'
-import { redirect } from 'next/navigation'
-import { NextResponse } from 'next/server'
-
-export default withAuth(
-    function middleware(request: NextRequestWithAuth) {
-        
-    },
-    {
-        callbacks: {
-            // authorized: ({ token }) => token?.role === 'admin'
-        }
-    }
-)
-
+export { default } from 'next-auth/middleware'
 export const config = { matcher: ["/contractor-dashboard","/customers-dashboard","/admin-dashboard","/customers","/estimates","/settings/:path*","/customerform","/estimateform"] }
