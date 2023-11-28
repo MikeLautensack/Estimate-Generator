cfe379684fad531089745774824491afa8277182
Mike <mikelautensack100@gmail.com>
2023-11-27 23:36:44 -0500

    Finished porting back to route handlers

--- estimate_generator/src/app/api/customers/create/route.ts
+++ estimate_generator/src/app/api/customers/create/route.ts
@@ -8,6 +8,7 @@ export async function POST(request: NextRequest) {
     const data = await request.json()
     const session = await getServerSession(authOptions)
     try {
+
         await db.insert(customers).values({
             id: Math.floor(Math.random() * 100000000),
             name: data.name,
@@ -15,8 +16,9 @@ export async function POST(request: NextRequest) {
             phone: data.phone,
             email: data.email,
             contractor_user_id: session.user.id,
-            customer_user_id: data.customerUserID,
+            customer_user_id: data.customer_user_id,
         })
+
         return NextResponse.json('Customer sucsessfully created')
     } catch (error) {
         return NextResponse.json(error)
