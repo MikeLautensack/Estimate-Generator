1c2e77670fbb8532c90771f9b0e7e4891295fda0
Mike <mikelautensack100@gmail.com>
2024-01-29 00:11:31 -0500

    Bug 2 Attempt 1

--- estimate_generator/src/app/(contractor)/contractor-dashboard/customers/form/[id]/page.tsx
+++ estimate_generator/src/app/(contractor)/contractor-dashboard/customers/form/[id]/page.tsx
@@ -2,6 +2,7 @@ import CustomerForm from '@/components/forms/CustomerForm'
 import { db } from '../../../../../../db'
 import { customers } from '../../../../../../db/schemas/customers'
 import { eq } from "drizzle-orm"
+import { Customers } from '@/types/customers'
 
 async function getData(id: number) {
   const res = await db.select()
@@ -11,7 +12,7 @@ async function getData(id: number) {
 }
 
 export default async function page({ params }: { params: { id: string } }) {
-  const data = await getData(parseInt(params.id))
+  const data = await getData(parseInt(params.id)) as Customers[]
   return (
     <main className='bg-neutral400 p-4 min-h-[calc(100vh-56px)] flex flex-col justify-start items-start flex-1'>
         <h1 className='text-2xl desktop:text-[42px] font-bold text-black'>Customer Form</h1>
