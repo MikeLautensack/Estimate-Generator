f08f634595b34f2969ed24bfcb5cb3847be99788
Mike <mikelautensack100@gmail.com>
2024-01-29 00:15:51 -0500

    Bug 3 Attempt 1

--- estimate_generator/src/app/(contractor)/contractor-dashboard/customers/form/page.tsx
+++ estimate_generator/src/app/(contractor)/contractor-dashboard/customers/form/page.tsx
@@ -1,13 +1,15 @@
 import CustomerForm from "@/components/forms/CustomerForm";
+import { Customers } from "@/types/customers";
 
 export default async function Page() {
+    const customer = {} as Customers
     return (
       <main className='bg-neutral400 p-4 min-h-[calc(100vh-56px)] flex flex-col justify-start items-start flex-1'>
         <h1 className='text-2xl desktop:text-[42px] font-bold text-black'>Customer Form</h1>
         <div
           className='flex justify-center items-center flex-1 w-full'
         >
-          <CustomerForm data={null}/>
+          <CustomerForm data={customer}/>
         </div>
       </main>
     )
