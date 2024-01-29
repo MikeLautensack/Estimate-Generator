cfb501f0e98f9e8ec9e08bddb5d8dcb43301e6a0
Mike <mikelautensack100@gmail.com>
2024-01-28 23:57:20 -0500

    Deleted all instances of | null and | undefined from all types in and refactored all type files to .d.ts file extension.  Will debug from vercel build logs.

---
+++ estimate_generator/src/types/changeOrders.d.ts
@@ -0,0 +1,54 @@
+export type ChangeOrders = {
+    id: number,
+    changeOrderName: string,
+    estimateName: string,
+    description: string,
+    customerName: string,
+    projectAddress: string,
+    status: string,
+    estimate_id: number,
+    dateCreated: Date,
+    dateUpdated: Date,
+    contractor_user_id: number,
+    customer_user_id: number,
+}
+
+export type ChangeOrderFormParams = {
+    estimateName: string | string[],
+    customerName?: string | string[],
+    projectAddress: string | string[],
+    mode: string,
+    contractor_user_id: number,
+    estimate_id?: number,
+    customer_user_id: number,
+    change_order_id?: number
+}
+
+export type ChangeOrderForm = {
+    data: ChangeOrderFormParams,
+}
+
+export type ChangeOrderFormProps = {
+    changeOrderName: string,
+    description: string,
+}
+
+export type ChangeOrderRequestsProps = {
+    changeOrders: ChangeOrders[],
+}
+
+export type ChangeOrderRequestProps = {
+    changeOrder: ChangeOrders
+}
+
+export type ChangeOrderRequestRowProps = {
+    orderRequest: ChangeOrderRequest,
+    setOrdersSelectedState: any,
+    ordersSelectedState: any
+    id: number
+}
+export type ChangeOrderRequest = {
+    name: string,
+    description: string,
+    status: string
+}
