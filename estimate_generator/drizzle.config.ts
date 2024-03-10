825a46efe546e33ada1b501471b3618fcd0cbd9a
Mike <mikelautensack100@gmail.com>
2024-03-10 15:46:50 -0400

    Utils folder is refactored.

--- estimate_generator/drizzle.config.ts
+++ estimate_generator/drizzle.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "drizzle-kit"
-import dotenv from "dotenv"
+import type { Config } from "drizzle-kit";
+import dotenv from "dotenv";
 
-dotenv.config({ path: ".env.local" })
+dotenv.config({ path: ".env.local" });
 
 export default {
   schema: "./src/db/schemas/*",
   out: "./src/db/drizzle",
-  driver: 'mysql2',
+  driver: "mysql2",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   }
