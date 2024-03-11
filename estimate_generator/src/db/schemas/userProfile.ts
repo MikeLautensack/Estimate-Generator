0a5b44e7a72895ef1667be5c60de993273627faf
Mike <mikelautensack100@gmail.com>
2024-03-10 23:47:58 -0400

    Refactored userProfile schema.

--- estimate_generator/src/db/schemas/userProfile.ts
+++ estimate_generator/src/db/schemas/userProfile.ts
@@ -1,19 +1,19 @@
-import {
-  mysqlTable,
+import { 
+  pgTable, 
   varchar,
   bigint,
   timestamp,
-} from "drizzle-orm/mysql-core"
+} from "drizzle-orm/pg-core";
 
-export const profiles = mysqlTable(
+export const profiles = pgTable (
     "profiles", 
 {
-  id: bigint('id', { mode: 'number' }).notNull().primaryKey(),
-  businessName: varchar('business_name', { length: 255 }),
-  businessAddress: varchar('business_address', { length: 255 }),
-  businessEmail: varchar('business_email', { length: 255 }),
-  businessPhone: varchar('business_phone', { length: 255 }),
+  id: bigint("id", { mode: "number" }).notNull().primaryKey(),
+  businessName: varchar("business_name", { length: 255 }),
+  businessAddress: varchar("business_address", { length: 255 }),
+  businessEmail: varchar("business_email", { length: 255 }),
+  businessPhone: varchar("business_phone", { length: 255 }),
   dateCreated: timestamp("date_created", { mode: "date" }).notNull(),
   dateUpdated: timestamp("date_updated", { mode: "date" }).notNull(),
-  user_id: bigint('user_id', { mode: 'number' })
-})
+  user_id: bigint("user_id", { mode: "number" })
+});
