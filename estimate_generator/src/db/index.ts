92194cf0e52196237712807c633d3582d31f11b4
Mike <mikelautensack100@gmail.com>
2024-03-10 23:07:47 -0400

    Removing nm dir.

--- estimate_generator/src/db/index.ts
+++ estimate_generator/src/db/index.ts
@@ -1,11 +1,11 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import dotenv from "dotenv";
 
 dotenv.config({ path: ".env.local" });
  
 // create the connection
-const sql = neon(process.env.NEON_DB!)
+const sql = neon(process.env.NEON_DB!);
 const db = drizzle(sql);
 export type DbClient = typeof db;
 
