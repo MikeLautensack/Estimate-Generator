91c4031de1a2298411e7f61e2e8e306e59dda669
Mike <mikelautensack100@gmail.com>
2024-05-17 03:18:42 -0400

    Fixed bug on password validation test by adding the act func and awaiting the events correctly.

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -2,49 +2,61 @@
 
 import React from "react";
 import { useState } from "react";
-import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { LoginFormValues } from "@/types/types";
 import { onSubmit } from "@/utils/formUtils/signInForm";
-import { Button } from "../ui/button";
+import signInSchema from "@/validations/signInSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, TextField, Typography } from "@mui/material";
 
 const SignInForm = () => {
   const [eyeOpen, setEyeOpen] = useState(false);
-  const { register, handleSubmit } = useForm<LoginFormValues>();
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
   return (
     <form
       className="bg-blue-100 m-8 p-4 rounded-xl w-4/5 tablet:w-3/5 desktop:w-1/2 max-w-xl"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <h1 className="text-[32px] font-bold text-black text-center">
-        Welcome Back
-      </h1>
-      <p className="text-base text-normal text-black text-center">
-        Please log in to continue
-      </p>
+      <Typography>Welcome Back</Typography>
+      <Typography>Please log in to continue</Typography>
       <div className="my-2">
-        <label className="">Email Address</label>
-        <input
-          className="w-full rounded p-1"
-          {...register("email", { required: true })}
-        ></input>
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Email Address"
+              error={!!errors.email}
+              helperText={errors.email?.message}
+            />
+          )}
+        />
       </div>
       <div className="my-2">
-        <label className="">Password</label>
-        <div className="relative">
-          <input
-            className="w-full rounded p-1"
-            type={eyeOpen ? "text" : "password"}
-            {...register("password", { required: true })}
-          ></input>
-          <Button
-            className="absolute top-2 right-2"
-            onClick={() => setEyeOpen(!eyeOpen)}
-          >
-            {eyeOpen ? <FaRegEye /> : <FaRegEyeSlash />}
-          </Button>
-        </div>
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Password"
+              error={!!errors.password}
+              helperText={errors.password?.message}
+            />
+          )}
+        />
       </div>
       <div className="flex justify-between my-2">
         <div className="flex gap-2">
@@ -53,8 +65,12 @@ const SignInForm = () => {
         </div>
         <Button className="text-black">Forgot Password?</Button>
       </div>
-      <Button className="w-full bg-blue-500 text-secondary500 py-2 rounded">
-        Log In
+      <Button
+        className="w-full bg-blue-500 text-secondary500 py-2 rounded"
+        type="submit"
+        variant="text"
+      >
+        Sign In
       </Button>
       <div id="divider" className="w-full border border-black my-4"></div>
       <div className="flex gap-1 justify-center">
