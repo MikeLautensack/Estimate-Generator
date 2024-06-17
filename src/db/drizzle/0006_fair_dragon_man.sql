684ed17e6a839ec73f65cb9e37b05c6e9bc1f999
Mike <mikelautensack100@gmail.com>
2024-06-17 00:31:40 -0400

    I had thought there was a problem with the relationship defs, however I had a duplicate table name that was causing the customers schema to not update correctly.

--- src/db/drizzle/0006_fair_dragon_man.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "profiles" ALTER COLUMN "business_name" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "profiles" ALTER COLUMN "business_address" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "profiles" ALTER COLUMN "business_email" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "profiles" ALTER COLUMN "business_phone" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "profiles" ALTER COLUMN "user_id" SET NOT NULL;
--- src/db/schemas/workOrders.ts
+++ src/db/schemas/workOrders.ts
@@ -1,6 +1,6 @@
 import { pgTable, varchar, bigint, timestamp } from "drizzle-orm/pg-core";
 
-export const workOrders = pgTable("customers", {
+export const workOrders = pgTable("workOrders", {
   id: bigint("id", { mode: "number" }).notNull().primaryKey(),
   contractor_user_id: bigint("contractor_user_id", {
     mode: "number",
