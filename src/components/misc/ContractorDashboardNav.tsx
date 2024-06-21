7dfe16c0836f09aacb0c7bdf8ae53deadf8e42a5
Mike <mikelautensack100@gmail.com>
2024-06-21 00:51:55 -0400

    added a dark mode switch to the new header.

--- src/components/misc/ContractorDashboardNav.tsx
+++ src/components/misc/ContractorDashboardNav.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import AccountMenu from "./AccountMenu";
 import NotificationsMenu from "./NotificationsMenu";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 const ContractorDashboardNav = () => {
   return (
     <div
       id="contractor-dashboard-nav"
-      className="flex justify-center items-center gap-4"
+      className="flex justify-center items-center gap-2"
     >
       <NotificationsMenu />
       <AccountMenu />
+      <DarkModeSwitch />
     </div>
   );
 };
