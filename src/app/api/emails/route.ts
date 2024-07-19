37937661a4c22d095c85e5d62a2fa87f58be3a83
MikeLautensack <mikelautensack100@gmail.com>
2024-07-19 18:16:29 -0400

    updating the sendVerificationRequest function to get it to work in prod. x5

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -7,6 +7,18 @@ import { Resend } from "resend";
 export async function POST(request: NextRequest) {
   // Get request body data
   const bodyData = await request.json();
+
+  // Validate the request body data
+  if (
+    !bodyData ||
+    !bodyData.emailType ||
+    !bodyData.from ||
+    !bodyData.identifier ||
+    !bodyData.subject
+  ) {
+    throw new Error("Missing required fields in the request body.");
+  }
+
   // Instanciate resend
   const resend = new Resend(process.env.EMAIL_KEY);
 
@@ -22,6 +34,8 @@ export async function POST(request: NextRequest) {
     case "updated-estimate":
       react = UpdatedEstimateEmail;
       break;
+    default:
+      throw new Error("Invalid email type.");
   }
 
   // Send email
@@ -44,8 +58,16 @@ export async function POST(request: NextRequest) {
       { status: 200 },
     );
   } catch (error: any) {
+    console.error("Error sending email:", error);
+
+    // Additional logging for debugging
+    console.error("Request body:", request.body);
+
     return NextResponse.json(
-      { error: error, message: `Error: ${error.message}` },
+      {
+        error: error.message || "Internal Server Error",
+        message: `Error: ${error.message}`,
+      },
       { status: 500 },
     );
   }
